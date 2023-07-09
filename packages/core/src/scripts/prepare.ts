952faa7b6225aabce16e37cb87abffee44acbfcc
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2023-07-10 04:28:38 +0530

    ...: resolved cached

---
+++ packages/core/src/scripts/prepare.ts
@@ -0,0 +1,84 @@
+import shell from "shelljs";
+
+class EnpCript {
+    constructor(private keystoreLocation: string) {
+        const isGPGInstalled = shell.which("ssh-keygen");
+        if (!isGPGInstalled) {
+            console.log("ssh-keygen is not installed. Please install ssh-keygen and try again.");
+        }
+        if (!shell.test("-e", keystoreLocation)) {
+            shell.mkdir("-p", keystoreLocation);
+        }
+    }
+
+    public generateKey(keyname: string) {
+        // Check if keyname exists
+        if (shell.test("-e", `${this.keystoreLocation}/${keyname}.key`)) {
+            return "Key already exists.";
+        }
+
+        // Generate key
+        console.log(`${this.keystoreLocation}/${keyname}`);
+        shell.exec(
+            `
+            ssh-keygen -t rsa -P "" -b 4096 -m PEM -f ${this.keystoreLocation}/${keyname}.key
+        `,
+            {
+                silent: false,
+            },
+        );
+        shell.exec(
+            `
+            ssh-keygen -e -m PEM -f ${this.keystoreLocation}/${keyname}.key > ${this.keystoreLocation}/${keyname}.key.pub
+        `,
+            {
+                silent: false,
+            },
+        );
+
+        return "Key generated successfully.";
+    }
+
+    public encryptMessage(message: string, keyname: string) {
+        // Check if keyname exists
+        if (!shell.test("-e", `${this.keystoreLocation}/${keyname}.key`)) {
+            return;
+        }
+
+        // Encrypt message
+        const encData = shell.exec(
+            `
+            echo "${message}" | openssl pkeyutl -encrypt -pubin -inkey ${this.keystoreLocation}/${keyname}.key.pub | base64
+        `,
+            {
+                silent: false,
+            },
+        );
+
+        return encData.stdout;
+    }
+
+    public decryptMessage(message: string, keyname: string) {
+        // Check if keyname exists
+        if (!shell.test("-e", `${this.keystoreLocation}/${keyname}.key`)) {
+            return;
+        }
+
+        // Decrypt message
+        const decData = shell.exec(
+            `
+            echo "${message}" | base64 --decode | openssl pkeyutl -decrypt -inkey ${this.keystoreLocation}/${keyname}.key
+        `,
+            {
+                silent: false,
+            },
+        );
+
+        if (decData.stderr) {
+            throw new Error(decData.stderr);
+        }
+        return decData.stdout;
+    }
+}
+
+export default EnpCript;
