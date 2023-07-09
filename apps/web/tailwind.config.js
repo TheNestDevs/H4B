4f87a3d2c81e7abf2a9daaccfed9adb2bcf906ce
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-09 13:04:27 +0530

    updated color theme

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -9,9 +9,9 @@ module.exports = {
         extend: {
             colors: {
                 primary: "rgb(var(--color-primary) / <alpha-value>)",
-                secondary: "rgb(var(--color-secondary) / <alpha-value>)",
                 accent: "rgb(var(--color-accent) / <alpha-value>)",
                 text: "rgb(var(--color-text) / <alpha-value>)",
+                "text-secondary": "rgb(var(--color-text-secondary) / <alpha-value>)",
                 background: "rgb(var(--color-background) / <alpha-value>)",
             },
         },
