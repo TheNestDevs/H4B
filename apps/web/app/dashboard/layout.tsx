1b5d4d4f94fe3af7ee247c251d427980909d04ee
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-09 13:42:32 +0530

    removed navbar from dashboard layout

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -10,7 +10,6 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
     return (
         <>
             <div className="min-h-full">
-                <Navbar />
                 <main className="pt-10">
                     <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">{children}</div>
                 </main>
