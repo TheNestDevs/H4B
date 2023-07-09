a313681b368e08b0dd8e5198c0ddfbb67cecdbdd
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-09 23:16:28 +0530

    complted video chat ui

--- apps/web/app/meet/[id]/page.tsx
+++ apps/web/app/meet/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { PhoneCall, User2 } from "lucide-react";
+import { PhoneCall, User2, Video, Mic, PhoneOff } from "lucide-react";
 import Button from "@/components/ui/Button";
 
 const MeetPage = () => {
@@ -8,15 +8,15 @@ const MeetPage = () => {
         <div className={"relative flex h-screen w-screen items-center justify-center bg-black"}>
             <User2 className={"h-20 w-20 text-white"} />
             <div className="user-video absolute bottom-10 right-10 h-52 w-80 rounded-lg bg-gray-900"></div>
-            <div className="controls absolute bottom-10  flex gap-2">
-                <Button className={"bg-green p rounded-full px-5 py-5 text-white"}>
-                    <PhoneCall className={"h-6 w-6"} />
+            <div className="controls absolute bottom-10 flex gap-6">
+                <Button className={"p rounded-full bg-red-200 px-5 py-5 text-red-800"}>
+                    <PhoneOff className={"h-6 w-6"} />
                 </Button>
                 <Button className={"bg-green rounded-full px-5 py-5 text-white"}>
-                    <PhoneCall className={"h-6 w-6"} />
+                    <Video className={"h-6 w-6"} />
                 </Button>
                 <Button className={"bg-green rounded-full px-5 py-5 text-white"}>
-                    <PhoneCall className={"h-6 w-6"} />
+                    <Mic className={"h-6 w-6"} />
                 </Button>
             </div>
         </div>
