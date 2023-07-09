6d313b54624229d81dfba0d2f1edddf6bec1b95e
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-10 00:46:47 +0530

    video chat

--- apps/web/app/meet/[id]/page.tsx
+++ apps/web/app/meet/[id]/page.tsx
@@ -1,10 +1,30 @@
 "use client";
 
+import { useState } from "react";
+
 import { Mic, PhoneOff, User2, Video } from "lucide-react";
+import AgoraUIKit from "agora-react-uikit";
 
 import Button from "@/components/ui/Button";
 
 const MeetPage = () => {
+    const [videoCall, setVideoCall] = useState(false);
+
+    const rtcProps = {
+        appId: "d37f8e2e8aeb44b1aba9a911d957b666",
+        channel: "test",
+        token: "007eJxTYPi3fecUi5gb/7LCv4es2VltIDtVo3DrVX6md416bK5X0ooVGNJMTI2SzSzMzNNMU0zMk0wtEhPNk5PSjM1MTC0Mjc1SEv+sSmkIZGS4Nes1MyMDBIL4LAwlqcUlDAwAqO8hIQ==",
+    };
+    const callbacks = {
+        EndCall: () => setVideoCall(false),
+    };
+    return videoCall ? (
+        <div className={"video relative flex h-screen w-screen"}>
+            <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+        </div>
+    ) : (
+        <h3 onClick={() => setVideoCall(true)}>Join</h3>
+    );
     return (
         <div className={"relative flex h-screen w-screen items-center justify-center bg-black"}>
             <User2 className={"h-20 w-20 text-white"} />
