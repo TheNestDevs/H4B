8bd2a964cbf9f6012ba1cfe8e99d03e316caf3f9
DarkFalc0n <unfezant211@gmail.com>
2023-07-10 04:38:12 +0530

    setup parser

--- packages/core/src/libs/symp_response_parser.ts
+++ packages/core/src/libs/symp_response_parser.ts
@@ -1,43 +1,54 @@
 export function isNumber(char: string) {
-    return /^\d$/.test(char);
+  return /^\d$/.test(char);
 }
 
 interface Response {
-    disease: string;
-    likelihood: number;
-    consultation: string[];
+  disease: string;
+  likelihood: number;
+  consultation: string[];
 }
 
 const parer = async (input: string) => {
-    const regex = /(.+)\s\((\d+)% likelihood\) - Consult (a|an) (.+)/;
-    const matches = RegExp(regex).exec(input);
-  
-    if (matches && matches.length === 4) {
-      const x = matches[1].trim();
-      const y = parseInt(matches[2]);
-      const z = matches[3].trim();
-  
-      return { x, y, z };
-    } else {
-      throw new Error('Invalid input format');
-    }
-}
+  const regex = /(.+)\s\((\d+)% likelihood\) - Consult (a|an) (.+)/;
+  const matches = RegExp(regex).exec(input);
 
+  if (matches && matches.length === 4) {
+    const x = matches[1].trim();
+    const y = parseInt(matches[2]);
+    const z = matches[3].trim();
+
+    return { x, y, z };
+  } else {
+    throw new Error("Invalid input format");
+  }
+};
+
+interface Disease {
+  name: string;
+  likelihood: string;
+  specialist: string;
+}
 export default async (response: string) => {
-    console.log(new Buffer(response).toString('base64'))
-    const regex = /(.+)\s\((\d+)% likelihood\) - Consult (a|an) (.+)/gm;
-    const matches = response.matchAll(regex);
-    console.log(matches)
-    const result = [];
+  let diseaseList: Disease[] = [];
+  let sentences = response.split("-");
+  let cleansedSentences = sentences.map((e) => e.replace(/\n/g, "").trim());
+  cleansedSentences.shift();
+  let mergedSentences = [];
+  for (let i = 0; i < cleansedSentences.length; i += 2) {
+    mergedSentences.push(cleansedSentences[i] + " " + cleansedSentences[i + 1]);
+  }
+  // console.log(mergedSentences)
+  const regExp = /(.*)(\((\d+)% likelihood)\) (C|c)onsult an* (.*)/gm;
+  mergedSentences.map((sentence) => {
+    let matches = sentence.matchAll(regExp);
+    let disease: Partial<Disease> = {};
 
     for (const match of matches) {
-        console.log(match)
-        const x = match[1].trim();
-        const y = parseInt(match[2]);
-        const z = match[5].trim();
-
-        result.push({ x, y, z });
+      disease.name = match[1];
+      disease.likelihood = match[3];
+      disease.specialist = match[5];
     }
-
-    return result;
-}
+    diseaseList.push(disease as Disease);
+  });
+  return diseaseList;
+};
