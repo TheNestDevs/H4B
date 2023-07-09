8dc4efb39c578d48e2be28ce66dca2aecf32a76e
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-09 13:25:35 +0530

    added prettier ignore

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -7,17 +7,17 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
     title: "Nextjs 13, TailwindCss Template",
     description: "A template for frontend developers created By Chirag Bhalotia",
-    manifest: "/manifest.json"
+    manifest: "/manifest.json",
 };
 
 interface RootLayoutProps {
     children: React.ReactNode;
 }
 
-export default function RootLayout({children}: RootLayoutProps) {
+export default function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="en">
-        <body className={`${inter.className} text-text`}>{children}</body>
+            <body className={`${inter.className} text-text`}>{children}</body>
         </html>
     );
 }
