f74611364479ddb9e3b9ae55eff350bdd7e54df2
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-09 16:52:49 +0530

    format and linting

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+
 import Navbar from "@/components/ui/Navbar";
 
 interface DashboardLayoutProps {
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,8 @@
+import React from "react";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
-import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
