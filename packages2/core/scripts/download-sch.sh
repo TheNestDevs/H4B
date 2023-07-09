4b1ccaa5413772db219238b2184eff7740c7995a
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2023-07-10 04:23:22 +0530

    ...

--- packages2/core/scripts/download-sch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source .env
-FILE=graphql/schema.graphql
-rm -rf $FILE
-pnpm gq $HASURA_GRAPHQL_ENDPOINT/v1/graphql -H "X-Hasura-Admin-Secret: $HASURA_GRAPHQL_ADMIN_SECRET" --introspect > $FILE
