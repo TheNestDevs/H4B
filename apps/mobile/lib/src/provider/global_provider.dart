bb20c4a3a4d77ec128bbdb3cda89c9e8dc7f645b
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-07-10 16:55:28 +0530

    Implemented video call

--- apps/mobile/lib/src/provider/global_provider.dart
+++ apps/mobile/lib/src/provider/global_provider.dart
@@ -4,5 +4,4 @@ final userNameProvider = Provider<String>((ref) => 'Rahul Rajput');
 
 final doctorNameProvider = Provider<String>((ref) => 'Dr. Corti');
 
-final baseUrlProvider =
-    Provider<String>((ref) => 'https://5b91-203-171-240-120.ngrok-free.app');
+final baseUrlProvider = Provider<String>((ref) => 'https://api.zenithmed.xyz');
